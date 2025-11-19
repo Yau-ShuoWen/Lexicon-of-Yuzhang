d0428badd10ee59a8169330d02882705ce7e8c88
steamed twisted rolls <yausw13@gmail.com>
2025-11-19 16:02:13 +0800

    🪛 重构
    在做拼音的时候总是会碰见这个，然后习惯性的修改，代码格式化，就很难过，所以重构了一下修改的流程，用好循环，而不是非常笨的识别

--- src/main/java/com/shuowen/yuzong/Linguistics/Mandarin/HanPinyin.java
+++ src/main/java/com/shuowen/yuzong/Linguistics/Mandarin/HanPinyin.java
@@ -7,8 +7,11 @@ import com.hankcs.hanlp.dictionary.py.PinyinDictionary;
 
 import java.util.*;
 
+import static com.shuowen.yuzong.Tool.StringTool.replace;
+
 /**
  * 所有汉语拼音有关内容都放在这里， Pinyin类不要用在外面，外面直接用字符串接住
+ * @apiNote 由于Hanlp使用5表示轻声，这里不做修改，0声是无效的
  * */
 
 public class HanPinyin
@@ -24,15 +27,17 @@ public class HanPinyin
      *  */
     public static List<String> txtPinyin(String txt)
     {
-        List<Pinyin> a= HanLP.convertToPinyinList(txt);
-        List<String> ans=new ArrayList<>();
-        if(a.size()!=txt.length()) throw new RuntimeException("数量不对应");
-        for(int i=0;i<a.size();i++)
+        List<Pinyin> a = HanLP.convertToPinyinList(txt);
+        List<String> ans = new ArrayList<>();
+        if (a.size() != txt.length()) throw new RuntimeException("数量不对应");
+        for (int i = 0; i < a.size(); i++)
         {
-            String c=txt.substring(i,i+1);
-            if("兀".equals(c)) { ans.add("wu4"); continue; }
-            if("嗀".equals(c)) { ans.add("hu4"); continue; }
-            ans.add(a.get(i).toString());
+            switch (txt.substring(i, i + 1))
+            {
+                case "兀" -> ans.add("wu4");
+                case "嗀" -> ans.add("hu4");
+                default -> ans.add(a.get(i).toString());
+            }
         }
         return ans;
     }
@@ -64,7 +69,6 @@ public class HanPinyin
 
     /**
      * 拼音去除标号  zhe4->zhe
-     * 沒有合法性檢查
      * */
     public static String toSyllable(String string)
     {
@@ -77,73 +81,66 @@ public class HanPinyin
 
     /**
      * 拼音轉注音標號  zhe4->4
-     * 沒有合法性檢查
+     * @apiNote 无效的时候使用-1做返回值
      * */
     public static int toTone(String str)
     {
-        if (str.equals("none5")) return 0;
+        if (str.length() <= 1) return -1;
 
-        if (str.isEmpty()) throw new RuntimeException("空字符串");
-        if (str.length() == 1) throw new RuntimeException("無效字符串");
-
-        char ans=str.charAt(str.length() - 1);
-        return (int)ans-'0';
+        int ans = str.charAt(str.length() - 1) - '0';
+        return (ans >= 1 && ans <= 5) ? ans : -1;
     }
 
     /**
-     * 一个电脑处理的拼音变常见拼音，把pin1yin1转换成pīnyīn
-     *
+     * 一个电脑处理的拼音变常见拼音，把{@code pin1} 转换成{@code pīn}
+     * @apiNote 无效的时候使用空字符串做返回值
      **/
-    public static String topMark(String string1)
+    public static String topMark(String pinyin)
     {
-        int tongue= toTone(string1);
-        if(tongue==0) return null;
-        StringBuilder str = new StringBuilder(string1);
+        StringBuilder str = new StringBuilder(pinyin);
         str.deleteCharAt(str.length() - 1);
 
-        for(int i=0;i<str.length();i++) if(str.charAt(i)=='v') str.setCharAt(i,'ü');
-        for(int i=0;i<str.length();i++) if(str.charAt(i)=='a')
-        {
-            char[] s = {' ','ā','á','ǎ','à','a'};
-            str.setCharAt(i,s[tongue]);
-            return str.toString();
-        }
-        for(int i=0;i<str.length();i++) if(str.charAt(i)=='o')
-        {
-            char[] s = {' ','ō','ó','ǒ','ò','o'};
-            str.setCharAt(i,s[tongue]);
-            return str.toString();
-        }
-        for(int i=0;i<str.length();i++) if(str.charAt(i)=='e')
-        {
-            char[] s = {' ','ē','é','ě','è','e'};
-            str.setCharAt(i,s[tongue]);
-            return str.toString();
-        }
-        for(int i=0;i<str.length();i++) if(str.charAt(i)=='i')
-        {
-            if(i==str.length()-1||str.charAt(str.length()-1)!='u')
-            {
+        int tongue = toTone(pinyin);
 
+        if ("none".contentEquals(str)||tongue == -1) return "";
+
+        replace(str, "v", "ü");
+
+        /* 使用的算法是基于简化描述：
+         *
+         * 1. 有iu组合时标记在u上，有ui组合时标记在i上
+         * 2. 依照a o e i u ü 的顺序标记在最先出现的字母上
+         * */
+
+        // 二维查找：map.get(元音).get(声调)
+        Map<String, List<String>> map = Map.of(
+                "a", List.of("", "ā", "á", "ǎ", "à", "a"),
+                "o", List.of("", "ō", "ó", "ǒ", "ò", "o"),
+                "e", List.of("", "ē", "é", "ě", "è", "e"),
+                "i", List.of("", "ī", "í", "ǐ", "ì", "i"),
+                "u", List.of("", "ū", "ú", "ǔ", "ù", "u"),
+                "ü", List.of("", "ǖ", "ǘ", "ǚ", "ǜ", "ü")
+        );
+
+        if (str.indexOf("iu") != -1)
+        {
+            replace(str, "u", map.get("u").get(tongue));
+        }
+        else if (str.indexOf("ui") != -1)
+        {
+            replace(str, "i", map.get("i").get(tongue));
+        }
+        else
+        {
+            for (String i : List.of("a", "o", "e", "i", "u", "ü"))
+            {
+                if (str.indexOf(i) != -1)
+                {
+                    replace(str, i, map.get(i).get(tongue));
+                    break;
+                }
             }
-            char[] s = {' ','ī','í','ǐ','ì','i'};
-            str.setCharAt(i,s[tongue]);
-            return str.toString();
         }
-        for(int i=0;i<str.length();i++) if(str.charAt(i)=='u')
-        {
-            char[] s = {' ','ū','ú','ǔ','ù','u'};
-            str.setCharAt(i,s[tongue]);
-            return str.toString();
-        }
-        for(int i=0;i<str.length();i++) if(str.charAt(i)=='u')
-        {
-            char[] s = {' ','ǖ','ǘ','ǚ','ǜ','ü'};
-            str.setCharAt(i,s[tongue]);
-            return str.toString();
-        }
-        char[] s = {' ','ˉ','ˊ','ˇ','ˋ',' '};
-        str.append(s[tongue]);
         return str.toString();
     }
 
