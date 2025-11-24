4bcd33bd5b66ae41cec72b1cd2e79c6a0ccd2897
steamed twisted rolls <yausw13@gmail.com>
2025-11-24 21:23:21 +0800

    ✏️ 新增权限

--- src/main/java/com/shuowen/yuzong/dao/domain/Account/Authority.java
+++ src/main/java/com/shuowen/yuzong/dao/domain/Account/Authority.java
@@ -12,20 +12,27 @@ public enum Authority
      * 管理员权限
      * */
 
-    /*
-    EDIT_ENTRY(),
-    AUDIT_ENTRY(),
-    SENSITIVE(),
-    FEEDBACK(),
-    PROOFREAD(),
-    ADMIN(),
-    */
-    ;
 
-    private int code;
+    EDIT("edit"),
+    AUDIT("audit"),
+    SENSITIVE("sensitive"),
+    FEEDBACK("feecback"),
+    PROOFREAD("proofread"),
+    ADMIN("admin"),
+    STUDY("study"),
+    NO("no");
 
-    Authority(int code)
+    private String code;
+
+    Authority(String code)
     {
-        this.code = code;
+        this.code = code.trim().toLowerCase();
+    }
+
+    public static Authority of(String code)
+    {
+        for (var i:values())
+            if(i.code.equals(code)) return i;
+        return NO;
     }
 }
