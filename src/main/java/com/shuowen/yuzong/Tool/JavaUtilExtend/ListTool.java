aae344ea66fd3279ca8a80802b04f4cb64c7e0bb
steamed twisted rolls <yausw13@gmail.com>
2025-12-31 19:17:45 +0800

    🪛 重构：
    自己摸索出的一个有趣的工具
    
    🛞 ListTool
    
    后来我发现这对于数据库的东西有很好的处理
    
    https://chatgpt.com/c/6954f22e-28a0-8322-ab69-791ccdf64c21

--- src/main/java/com/shuowen/yuzong/Tool/JavaUtilExtend/ListTool.java
+++ src/main/java/com/shuowen/yuzong/Tool/JavaUtilExtend/ListTool.java
@@ -20,4 +20,14 @@ public class ListTool
         for (T i : list) if (check.apply(i)) result.add(i);
         return result;
     }
+
+    /**
+     * 当业务流程「按道理说」应该得到一个元素，但在不确定情况下，实际结果可能是 0 个或多个，就可以使用这个方法来校验。
+     */
+    public static <T> T checkSizeOne(List<T> list, String ifSmaller, String ifLarger)
+    {
+        NullTool.checkNotNull(list);
+        if (list.size() == 1) return list.get(0);
+        else throw new IllegalArgumentException(list.isEmpty() ? ifSmaller : ifLarger);
+    }
 }
