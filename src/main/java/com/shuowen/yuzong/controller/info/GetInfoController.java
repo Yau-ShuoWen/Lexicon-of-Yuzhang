2e7b8583220d4026a1cfca3375af70bac8d655f5
steamed twisted rolls <yausw13@gmail.com>
2026-03-18 23:50:37 +0800

    📤 获得信息的类
    今天交这么多不是清库存，就是为了能本本分分刷绿色方块
    
    原来单独一个类，现在合并了，所以没有新增文件
    
    一想到键值对，就想到数据库被打爆，就想到redis，就想到redis被打爆，就想到写死在代码里，就想到扩展性，权衡这一块

--- src/main/java/com/shuowen/yuzong/controller/info/GetInfoController.java
+++ src/main/java/com/shuowen/yuzong/controller/info/GetInfoController.java
@@ -1,17 +1,25 @@
 package com.shuowen.yuzong.controller.info;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.shuowen.yuzong.Tool.dataStructure.UString;
 import com.shuowen.yuzong.Tool.dataStructure.option.Dialect;
-import com.shuowen.yuzong.controller.APIResponse;
+import com.shuowen.yuzong.Tool.dataStructure.option.Language;
+import com.shuowen.yuzong.Tool.dataStructure.text.ScTcText;
+import com.shuowen.yuzong.Tool.format.JsonTool;
 import com.shuowen.yuzong.data.mapper.Character.HanziMapper;
 import com.shuowen.yuzong.data.mapper.Reference.RefMapper;
 import com.shuowen.yuzong.data.mapper.Word.WordMapper;
+import com.shuowen.yuzong.service.impl.KeyValueService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Map;
 
 /**
- * 获得字典信息
+ * 获得字典信息<br>
+ * 不用{@code APIResponse}的原因是，接收一个常量都失败的话，前端有没有那个报错信息都无所谓了
  */
 @RestController
 @RequestMapping ("/api/info")
@@ -26,22 +34,34 @@ public class GetInfoController
     @Autowired
     private RefMapper ck;
 
-    @GetMapping ("{dialect}/get-info-number")
-    public APIResponse<Map<String, Integer>> getInfoNumber(@PathVariable String dialect)
+    @GetMapping ("/get-num/{d}")
+    public Map<String, Integer> getInfoNumber(@PathVariable Dialect d)
     {
-        try
-        {
-            var d = Dialect.of(dialect).toString();
-            return APIResponse.success(Map.of(
-                    "hanzi_num", hz.findRowCountInHanziTable(d),
-                    "pinyin_num", hz.findRowCountInPinyinTable(d),
-                    "ciyu_num", cy.findRowCountInCiyuTable(d),
-                    "ref_num", ck.findRowCountInReferTable(d)
-            ));
-        } catch (Exception e)
-        {
-            e.printStackTrace();
-            return APIResponse.failure(e.getMessage());
-        }
+        var dialect = d.toString();
+        return Map.of(
+                "hanzi_num", hz.findRowCountInHanziTable(dialect),
+                "pinyin_num", hz.findRowCountInPinyinTable(dialect),
+                "ciyu_num", cy.findRowCountInCiyuTable(dialect),
+                "ref_num", ck.findRowCountInReferTable(dialect)
+        );
+    }
+
+    @GetMapping ("/get-text/{d}/{l}/{code}")
+    public UString welcome(@PathVariable Dialect d, @PathVariable Language l,
+                           @PathVariable String code)
+    {
+        if (code.equals("welcome")) return ScTcText.get(KeyValueService.get("website-greeting:" + d), d, l);
+
+        if (code.contains("search")) return ScTcText.get(KeyValueService.get(code), l);
+
+
+        return UString.of("-");
+    }
+
+    @GetMapping ("/get-menu/{code}")
+    public List<ScTcText> pronunTagList(@PathVariable String code)
+    {
+        return JsonTool.readJson(KeyValueService.get("types-of-pronunciation"),
+                new TypeReference<>() {},new ObjectMapper());
     }
 }
